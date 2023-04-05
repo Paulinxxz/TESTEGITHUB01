7b8a784f3e60c0d6dc982989ac1662b8352510cc
Paulinxxz <129889616+Paulinxxz@users.noreply.github.com>
2023-04-05 13:27:59 -0300

    alteração js

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 document.addEventListener( "click" , showcolor );
 
-document.getElementById( "botao" ).addEventListener( "click" , exibenome);
+document.getElementById( "somar" ).addEventListener( "click" , somar);
+
+document.getElementById( "dividir" ).addEventListener( "click" , dividir);
+
+document.getElementById( "multiplicar" ).addEventListener( "click" , multiplicar);
+
+document.getElementById( "subtrair" ).addEventListener( "click" , subtrair);
 
 function showcolor()
 {
@@ -19,27 +25,29 @@ function somar()
     let numero1 = document.getElementById( "number1" ).value;
     let numero2 = document.getElementById( "number2" ).value;
     let numero3 = parseFloat(numero1) + parseFloat(numero2)
-    document.getElementById( "resultado1" ).innerHTML = " = " numero1 + numero2;
+    document.getElementById( "resultado1" ).innerHTML = " = " + numero3;
 }
 
 function dividir()
 {
-    let numero3 = document.getElementById( "number3" ).value;
-    let numero4 = document.getElementById( "number4" ).value;
-    document.getElementById( "resultado2" ).innerHTML = numero3 / numero4
+    let numero4 = document.getElementById( "number3" ).value;
+    let numero5 = document.getElementById( "number4" ).value;
+    let numero6 = parseFloat(numero4) / parseFloat(numero5)
+    document.getElementById( "resultado2" ).innerHTML = " = " / numero6;
 }
 
 function multiplicar()
 {
-    let numero5 = document.getElementById( "number5" ).value;
-    let numero6 = document.getElementById( "number6" ).value;
-    document.getElementById( "resultado3" ).innerHTML = numero5 * numero6
+    let numero7 = document.getElementById( "number5" ).value;
+    let numero8 = document.getElementById( "number6" ).value;
+    let numero9 = parseFloat(numero7) * parseFloat(numero8)
+    document.getElementById( "resultado3" ).innerHTML = " = " * numero9;
 }
 
 function subtrair()
 {
-    let numero7 = document.getElementById( "number7" ).value;
-    let numero8 = document.getElementById( "number8" ).value;
-    document.getElementById( "resultado4" ).innerHTML = numero7 - numero8
-
+    let numero10 = document.getElementById( "number7" ).value;
+    let numero11 = document.getElementById( "number8" ).value;
+    let numero12 = parseFloat(numero10) - parseFloat(numero11)
+    document.getElementById( "resultado4" ).innerHTML = " = " - numero12;
 }
