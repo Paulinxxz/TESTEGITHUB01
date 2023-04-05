40e8ccc7da469c501591e67d9e31b52876242513
Paulinxxz <129889616+Paulinxxz@users.noreply.github.com>
2023-04-05 11:57:04 -0300

    alterações html e js

--- main.js
+++ main.js
@@ -14,10 +14,9 @@ function exibenome()
     document.getElementById( "nome" ).value = "";
 }
 
-function somarValores()
-{
+function somarValores(){
     var s1 = document.getElementById("txt1").value;
     var s2 = document.getElementById("txt2").value;
-    var s3 = s1 + s2;
+    var s3 = s1*s2;
     alert(s3);
 }
