4170138996e93eff20c2e1fe4dd2080b3b370d1e
Paulinxxz <129889616+Paulinxxz@users.noreply.github.com>
2023-04-05 12:00:07 -0300

    alteração js

--- main.js
+++ main.js
@@ -17,6 +17,6 @@ function exibenome()
 function somarValores(){
     var s1 = document.getElementById("txt1").value;
     var s2 = document.getElementById("txt2").value;
-    var s3 = s1*s2;
+    var s3 = s1 + s2;
     alert(s3);
 }
