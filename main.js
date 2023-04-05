6d5a5cd17148e210d8f3dc34ce870c54147079f4
Paulinxxz <129889616+Paulinxxz@users.noreply.github.com>
2023-04-05 09:17:32 -0300

    Alteracoes 2

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-alert("Seja bem vindo ao Github Pages");
+alert( "Seja bem vindo ao Github Pages");
 
-alert("Seja bem vindo ao Senai")
+alert( "Seja bem vindo ao Senai");
 
-alert("Bem vindo Paulo")
+alert( "Bem vindo Paulo");
